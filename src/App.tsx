77252d863dd63687a3ecb0831fbe7a39bb1736bd
Felix Orinda <forinda82@gmail.com>
2022-10-11 10:41:35 +0300

    Add github login

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,41 @@
-import React from 'react'
+import React from "react";
 import { io } from "socket.io-client";
+import LoginPage from "./pages/LoginPage";
 
 const App = () => {
-
   const socket = io("http://localhost:6200");
   React.useEffect(() => {
     socket.on("connect", () => {
       console.log("connected");
     });
-    socket.on("disconnect", (data:any) => {
+    socket.on("disconnect", (data: any) => {
       console.log("disconnected", data);
-    })
-
+    });
+  }, []);
+  const fetchUser = async () => {
+    const res = await fetch(
+      "http://localhost:6200/api/v1/auth/google/login/success",
+      {
+        credentials: "include",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          "Alllow-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Credentials": "true",
+        },
+      }
+    );
+    const data = await res.json();
+    console.log(data);
+  };
+  React.useEffect(() => {
+    fetchUser();
   }, []);
   return (
-    <div>App</div>
-  )
-}
+    <div>
+      <LoginPage />
+    </div>
+  );
+};
 
-export default App
+export default App;
