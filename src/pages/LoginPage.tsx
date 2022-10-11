3aa7cf6ae0ef3965c02194bb8df85aa3e99cff68
Felix Orinda <forinda82@gmail.com>
2022-10-11 15:06:47 +0300

    Add loan category filters

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -9,10 +9,10 @@ const LoginPage = () => {
   };
   const githubLogin = () => {
     window.open("http://localhost:6200/api/v1/auth/github", "_self");
-    };
+  };
   const logout = async () => {
     const res = await fetch("http://localhost:6200/api/v1/auth/logout", {
-        method:'GET',
+      method: "GET",
       credentials: "include",
       headers: {
         Accept: "application/json",
@@ -22,27 +22,50 @@ const LoginPage = () => {
       },
     });
     console.log(await res);
-    
   };
   return (
-    <div>
-      <button
-        onClick={googleLogin}
-        className="bg-red-400 text-white border py-2 px-4 rounded-lg"
-      >
-        <FontAwesomeIcon icon={faGoogle} /> Google Login
-      </button> <button
-        onClick={githubLogin}
-        className="bg-gray-400 text-white border py-2 px-4 rounded-lg"
-      >
-        <FontAwesomeIcon icon={faGithub} color="black"/> Github Login
-      </button>
-      <button
-        onClick={logout}
-        className="bg-red-400 text-white border py-2 px-4 rounded-lg"
-      >
-        <FontAwesomeIcon icon={faTrash} /> Logout
-      </button>
+    <div className="min-h-screen p-6 flex items-center justify-center">
+      <div className="flex gap-6 p-10 shadow shadow-slate-500 rounded-md">
+        <div className="flex flex-col gap-5">
+          <button
+            onClick={googleLogin}
+            className="bg-red-400 text-white border py-2 px-4 rounded-lg"
+          >
+            <FontAwesomeIcon icon={faGoogle} /> Google Login
+          </button>{" "}
+          <button
+            onClick={githubLogin}
+            className="bg-gray-400 text-white border py-2 px-4 rounded-lg"
+          >
+            <FontAwesomeIcon icon={faGithub} color="black" /> Github Login
+          </button>
+          {/* <button
+            onClick={logout}
+            className="bg-red-400 text-white border py-2 px-4 rounded-lg"
+          >
+            <FontAwesomeIcon icon={faTrash} /> Logout
+          </button> */}
+        </div>
+        <div className="h-full flex justify-center items-center bg-red-400">
+          <span className="rounded-full shadow-md p-2 mx-auto">or</span>
+        </div>
+        <div>
+          <form action="flex flex-col gap-4">
+            {["email", "password"].map((i) => (
+              <div className="flex flex-col">
+                <label className="capitalize">{i}</label>
+                <input
+                  type="text"
+                  name={i}
+                  placeholder={i}
+                  className="border px-4 py-1 rounded"
+                />
+              </div>
+            ))}
+            <button className="uppercase bg-blue-600 text-white w-full my-4 rounded-lg px-4 py-2 text-xl">Login</button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
